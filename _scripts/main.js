52c4876d7e88542276865ccabfe9d0e50e54c822
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2016-05-15 12:04:14 -0400

    Update Sass files

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,21 +1,22 @@
 // /repos/:owner/:repo/releases/latest
-var latestRelease = 'https://github.com/TheJaredWilcurt/scout-app/releases';
-$.getJSON(latestRelease, function (data) {
-		console.log(data);
-	});
-	$.ajax({
-	type: 'GET',
-	url: latestRelease,
-	data: data,
-	async: false,
-	beforeSend: function (xhr) {
-		if (xhr && xhr.overrideMimeType) {
-		  xhr.overrideMimeType('application/json;charset=utf-8');
-		}
-	},
-	dataType: 'json',
-	success: function (data) {
-		//Do stuff with the JSON data
-		console.log(data);
-	}
+var latestRelease = 'http://github.com/TheJaredWilcurt/scout-app/releases';
+$.get(latestRelease, function (data) {
+    console.log(data);
 });
+
+//$.ajax({
+//    type: 'GET',
+//    url: latestRelease,
+//    data: data,
+//    async: false,
+//    beforeSend: function (xhr) {
+//        if (xhr && xhr.overrideMimeType) {
+//          xhr.overrideMimeType('application/json;charset=utf-8');
+//        }
+//    },
+//    dataType: 'json',
+//    success: function (data) {
+//        //Do stuff with the JSON data
+//        console.log(data);
+//    }
+//});
